efcf790b4ea2b4850f4e05a2583c3714ab8d8d00
gab5987 <biell.gb4@gmail.com>
2022-07-17 12:46:47 -0300

    FEAT. Add Node.Js client

---
+++ webTest/index.js
@@ -0,0 +1,30 @@
+const mqtt = require('mqtt')
+
+const host = 'broker.hivemq.com'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+
+var topic = [ 'JjQZFhodDDghISIALBYQNS8/temperature',  'JjQZFhodDDghISIALBYQNS8/humidity' ]
+
+client.on('connect', () => {
+    console.log('Connected')
+
+    for (var i in topic) {
+        client.subscribe([topic[i]], () => {})
+    }
+})
+
+client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString())
+  })
